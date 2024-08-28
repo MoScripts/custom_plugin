9b5ecb3071c521750686cab017e8a862d9ebdd77
MoScripts <98230970+MoScripts@users.noreply.github.com>
2024-08-29 01:38:18 +0300

    commit

---
+++ src/main/java/com/gweam/custom_plugin/Custom_plugin.java
@@ -0,0 +1,17 @@
+package com.gweam.custom_plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Custom_plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
